10636c9459e155f25afd90adc74c015e0d4a33e6
German Garcia <germangarciagarcia@yahoo.com>
2024-03-03 11:36:08 -0500

    fdhdfh

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplication14.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
